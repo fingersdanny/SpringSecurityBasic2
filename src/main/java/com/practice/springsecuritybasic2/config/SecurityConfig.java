28f7c7c0cb296363dbf71c85951a59b1270b7e2b
isevou__ <kbry12@naver.com>
2023-12-25 09:15:32 +0900

    feat : InMemoryUserDetailsManager 설정 추가

--- src/main/java/com/practice/springsecuritybasic2/config/SecurityConfig.java
+++ src/main/java/com/practice/springsecuritybasic2/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -44,5 +49,57 @@ public class SecurityConfig {
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
+	}
+
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		/**
+		 * 1. 인메모리 유저 권한 설정.  withDefaultPasswordEncoder 사용 (deprecated)
+		 */
+		// UserDetails admin = User.withDefaultPasswordEncoder()
+		// 	.username("admin")
+		// 	.password("1234")
+		// 	.authorities("admin")
+		// 	.build();
+		//
+		// UserDetails user = User.withDefaultPasswordEncoder()
+		// 	.username("user")
+		// 	.password("1234")
+		// 	.authorities("read")
+		// 	.build();
+
+		/**
+		 * 2. Custom PasswordEncoder 사용 (권장되는 방식)
+		 *  UserDetails는 한번 만들어지고 나면 Setter가 닫혀 있어서 수정이 불가함
+		 *  이는 framework 설계 상 임의로 유저에 대한 정보를 바꾸지 않게 하기 위해서
+		 *  지금과 같이 구현할 경우 InMemoryUserDetailsManager가 실제 구현체를 담당하며
+		 *  loadUserByUsername(String Username) 메서드를 통해서 AuthenticationProvider를 통해 제공 받은 인증를 통해서 작동함
+		 */
+		UserDetails admin = User.withUsername("admin")
+			.username("admin")
+			.password("1234")
+			.authorities("admin")
+			.build();
+
+		UserDetails user = User.withUsername("user")
+			.username("user")
+			.password("1234")
+			.authorities("read")
+			.build();
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+
+	/**
+	 * Principal -> Authentication -> UsernamePasswordAuthenticationToken
+	 * AuthenticationProvider, AuthenticationProviderManager에서 사용되어 모든 인증이 성공적이었는지 확인할 때 반환 타입
+	 *
+	 * UserDetails -> User
+	 * UserDetailsManager, UserDetailsService에서 유저 정보를 불러 오기 위해 사용
+	 */
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
